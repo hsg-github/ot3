c6696fb676675d5e12234083b5025b4fb8f47f67
Jeff Segal <jeff.segal@hunterstrategy.net>
2015-12-04 13:58:06 -0500

    Added TimesheetService

--- server/api/services/TimesheetService.coffee
+++ server/api/services/TimesheetService.coffee
@@ -1,15 +1,36 @@
 'use strict'
 
+log = require('loglevel');
+
 svc = {}
 
 svc.getWeekStartDate = (d) ->
+  original = new Date(d)
   d = new Date(d)
   day = d.getDay()
   diff = d.getDate() - day + (if day == 0 then -6 else 1)
   # adjust when day is sunday
-  return new Date(d.setDate(diff))
+  d.setHours 0
+  d.setMinutes 0
+  d.setSeconds 0
+  d.setMilliseconds 0
+  d.setDate(diff)
+  log.trace original + ' starts on ' + d + ' (diff=' + diff + ')'
+  return d
 
-svc.getNextTimesheet = (task, user, supervisor) ->
+svc.getWeekEndDate = (d) ->
+  original = new Date(d)
+  startDate = @getWeekStartDate(d)
+  startDayOfMonth = startDate.getDate()
+  endDate = new Date(startDate.setDate(startDayOfMonth + 6))
+  endDate.setHours 23
+  endDate.setMinutes 59
+  endDate.setSeconds 59
+  endDate.setMilliseconds 999
+  log.trace original + ' ends on ' + endDate
+  return endDate
+
+svc.getNextTimesheet = (task, user, supervisor, date = new Date()) ->
   return null if task.isComplete()
 
 
--- server/api/shared/common.spec.js
+++ server/api/shared/common.spec.js
@@ -1,29 +1,26 @@
 var app = require('../../app');
 var request = require('supertest');
 var mongoose = require('mongoose');
+var log = require('loglevel');
+
+log.setLevel('debug');
 
 var chai = require("chai");
 var chaiAsPromised = require("chai-as-promised");
 should = chai.should();
-expect = chai.expect;
 chai.use(chaiAsPromised);
 //chai.config.includeStack = true;
+
+global.chaiAsPromised = chaiAsPromised;
+global.expect = chai.expect;
+global.AssertionError = chai.AssertionError;
+global.Assertion = chai.Assertion;
+global.assert = chai.assert;
 
 var Bluebird = require("bluebird");
 require('mongoose').Promise = Bluebird;
 Bluebird.onPossiblyUnhandledRejection(function(){});
 //mongoose.Promise = global.Promise;
-
-//global.chaiAsPromised = chaiAsPromised;
-//global.expect = chai.expect;
-//global.AssertionError = chai.AssertionError;
-//global.Assertion = chai.Assertion;
-//global.assert = chai.assert;
-//
-//global.fulfilledPromise = Q.resolve;
-//global.rejectedPromise = Q.reject;
-//global.defer = Q.defer;
-//global.waitAll = Q.all;
 
 User = require('../user/user.model');
 Project = require('../Project/Project.model');
